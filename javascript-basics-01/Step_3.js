102fa89bf49f91c0804dfda521836f90a8667e76
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-08-31 14:54:42 +0300

    step 3

--- javascript-basics-01/Step_3.js
+++ javascript-basics-01/Step_3.js
@@ -1,2 +1,4 @@
-var name = prompt(" please entre your name");
-alert("Hello I am " + name);
+document.addEventListener("DOMContentLoaded", () => {
+    let name = prompt(" please entre your name");
+    alert("Hello I am " + name);
+})
