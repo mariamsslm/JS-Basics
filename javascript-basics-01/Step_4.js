afe0e5e586b32af5b7a80994d7eaa32c66744bda
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-08-31 10:37:22 +0300

    Step 4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+var name = prompt("please entre your name");
+var surname = prompt("please enter your surname");
+var city = prompt("please enter your city ");
+alert("Hello Iam" + name +surname + "\n I live in" +city);
