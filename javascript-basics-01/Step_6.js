7e4784abb7d30e9063ad7e29419f1b4a9a4b8c18
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-08-31 14:37:51 +0300

    step 6

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,12 +1,12 @@
-function div() {
-   var number1 = parseFloat(document.getElementById("first_number").value);
-   var number2 = parseFloat(document.getElementById("second_number").value);
-   var result = number1 /number2 ;
+document.addEventListener("DOMContentLoaded", () => {
+   let number1 = prompt("entre number 1");
+   let number2 = prompt("entre number 2");
+   let result = number1 / number2;
 
-   document.getElementById("validate").value = result;
-   
+  
+
    alert(result);
 
 
 
-}
+})
--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded",()=>{ 
     let shoe_size = prompt("entre your shoe size");
-    let year = prompt("entre your age");
+    let year = prompt("entre your year");
     let result = (((shoe_size * 2) + 5) * 50) - year + 1766;
     
 
