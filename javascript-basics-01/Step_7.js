3c7b121dae7d05881bbf54e8f768cbb5bab7be79
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-08-31 14:29:09 +0300

    step8

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,8 +1,6 @@
-
-function total() {
+function total(shoe_size , year) {
     var shoe_size = parseInt(document.getElementById("shoe_size").value);
     var year = parseInt(document.getElementById("year").value);
-    
     var result = (((shoe_size * 2) + 5) * 50) - year + 1766;
     document.getElementById("validate").value = result;
 
---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let age=prompt("enter age")
+    if(age>18){
+        alert("over than 18")
+
+    }
+    else{
+        alert('under the 18')
+    }
+
+})
+
