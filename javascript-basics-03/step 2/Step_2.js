901a9761c14eba3e2403ecb811f71b571778d774
Mariamsuleiman <suleimanmariam591@gmail.com>
2023-09-01 10:58:35 +0300

    step 2

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,12 @@
+let links= document.querySelectorAll("a");
+let div= document.querySelector("#texte")
+links.forEach(link =>{
+    link.addEventListener("click" , (e)=>{
+        if (e.target.id === "hide"){
+            div.style.display = "none"
+        }
+        if (e.target.id === "show"){
+            div.style.display = "block"}
+
+    })
+})
